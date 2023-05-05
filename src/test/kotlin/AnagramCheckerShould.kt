e34857383e0edf7a7650083611b6c6e13e41b88d
Daniel <daniel.zappold@gmx.de>
2023-05-05 18:04:08 +0200

    :test_tube: introduce frequency mapping

--- src/test/kotlin/AnagramCheckerShould.kt
+++ src/test/kotlin/AnagramCheckerShould.kt
@@ -1,5 +1,7 @@
 import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.maps.shouldContain
 import org.junit.jupiter.api.MethodOrderer.Random
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestMethodOrder
 import org.junit.jupiter.api.parallel.Execution
@@ -13,7 +15,25 @@ class AnagramCheckerShould {
         (Text("Madam Curie") isAnagramOf Text("Radium came"))
             .shouldBeTrue()
     }
+
+    @Nested
+    class FrequencyMapShould {
+        @Test
+        fun `count a single letter once`() {
+            Text("a").toFrequencyMap() shouldContain (Letter('a') to Count(1))
+        }
+    }
 }
+
+private fun Text.toFrequencyMap(): Map<Letter, Count> {
+    TODO("Not yet implemented")
+}
+
+@JvmInline
+value class Letter(val letter: Char)
+
+@JvmInline
+value class Count(val count: Int)
 
 @JvmInline
 value class Text(val text: String)
